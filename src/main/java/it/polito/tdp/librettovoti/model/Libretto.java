ecfa971266b8462523d13d18790689664069e260
giuse <giuse@LAPTOP-DQB5UGG5.home-life.hub>
2021-03-15 10:13:01 +0100

    Trasformazione MVC

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -9,6 +9,38 @@ public class Libretto {
 	public void add(Voto v) {
 		this.voti.add(v);
 	}
+	public List<Voto>  listaVotiUguali(int punteggio){
+		List <Voto> risultato = new ArrayList<>();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio)
+			risultato.add(v);
+		}
+		return risultato;
+	}
+	public Libretto  votiUguali(int punteggio){
+		Libretto risultato = new Libretto();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio)
+			risultato.add(v);
+		}
+		return risultato;
+	}
+	/**
+	 * Ricerca un corso di cui è specificato il nome
+	 * Se il corso non esiste, restituisce null.
+	 * @param nomeCorso
+	 * @return
+	 */
+	public Voto ricercaCorso (String nomeCorso) {
+		Voto risultato= null;
+		for(Voto v : this.voti) {
+			if(v.getNome().equals(nomeCorso)) {
+				risultato=v;
+				break;
+			}	
+		}
+		return risultato;
+	}
 	public String toString() {
 		String s="";
 		for (Voto v: this.voti) {
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 
@@ -12,7 +13,12 @@ public class TestLibretto {
 		Voto voto2= new Voto("Fisica 1", 28, LocalDate.of(2019, 7,15));
 		libretto.add(voto2);
 		libretto.add(new Voto("Informatica", 24, LocalDate.of(2019, 9,15)));
+		libretto.add(new Voto("Chimica", 25, LocalDate.of(2019, 9,15)));
 		System.out.println(libretto);
+		List <Voto> venticinque= libretto.listaVotiUguali(25);
+		System.out.println(venticinque);
+		Libretto librettoventicinque= libretto.votiUguali(25);
+		System.out.println(librettoventicinque);
 	}
 
 }
